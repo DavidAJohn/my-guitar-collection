4b2da41142267eb3896cb86a98f41a7b6efdf31f
David John <davidajohn@gmail.com>
2021-03-27 23:17:54 +0000

    Added guitar image upload preview and validation

--- src/app/account/add-new-guitar/add-new-guitar.component.ts
+++ src/app/account/add-new-guitar/add-new-guitar.component.ts
@@ -1,6 +1,7 @@
-import { ItemStatus } from './../../shared/models/baseItem';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { mimeType as mimeTypeValidator } from '../../shared/helpers/mime-type.validator';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add-new-guitar',
@@ -9,9 +10,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddNewGuitarComponent implements OnInit {
   addNewForm: FormGroup;
+  imagePreview: string;
   isSubmitting = false;
+  fileUploadTouched = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, public sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.createAddNewForm();
@@ -27,7 +30,7 @@ export class AddNewGuitarComponent implements OnInit {
       description: ['', [Validators.required, Validators.maxLength(250)]],
       itemStatus: ['1', [Validators.required]],
       yearMade: ['', [Validators.max(2050), Validators.min(1900)]],
-      imagePath: [{value: 'An image can be uploaded later', disabled: true}],
+      imagePath: ['', [Validators.required], [ mimeTypeValidator ]],
       numStrings: ['', [Validators.max(8), Validators.min(1)]]
     });
   }
@@ -42,4 +45,22 @@ export class AddNewGuitarComponent implements OnInit {
       this.isSubmitting = false;
     }
   }
+
+  onImageSelected(event: Event) {
+    this.fileUploadTouched = true;
+    const file = (event.target as HTMLInputElement).files[0];
+
+    if (file !== null && file !== undefined) {
+      this.addNewForm.patchValue({ imagePath: file });
+      this.addNewForm.get('imagePath').updateValueAndValidity();
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result as string;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
 }
