7aa4edafc672489a053ed46972b93569a31351bf
David John <davidajohn@gmail.com>
2021-04-09 23:29:59 +0100

    Home page now shows recent guitars from database

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Guitar } from '../shared/models/guitar';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  recentGuitars$: Observable<Guitar[]>;
+  
+  constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
+    this.loadRecentGuitars();
   }
 
+  loadRecentGuitars() {
+    this.recentGuitars$ = this.homeService.loadRecentGuitars();
+  }
 }
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,9 +1,28 @@
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Injectable } from '@angular/core';
+import { Guitar } from '../shared/models/guitar';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  loadRecentGuitars(): Observable<Guitar[]> {
+    return this.firestore.collection<Guitar>("guitars", ref => ref
+      .where("itemStatus", "==", "1")
+      .orderBy("createdAt", "desc")
+      .limit(3))
+        .snapshotChanges().pipe(
+          map(actions => actions.map(a => {
+            const data = a.payload.doc.data() as Guitar;
+            return data;
+          })
+        )
+      )
+  }
+
 }
