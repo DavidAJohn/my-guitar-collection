e66f13440d2c419ac00b44986f1c395b70c995eb
David John <davidajohn@gmail.com>
2021-04-11 17:15:52 +0100

    Home page now shows guitars from the database

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CardActionType } from '../shared/components/guitar-card/guitar-card.component';
 import { Guitar } from '../shared/models/guitar';
 import { HomeService } from './home.service';
 
@@ -10,6 +11,7 @@ import { HomeService } from './home.service';
 })
 export class HomeComponent implements OnInit {
   recentGuitars$: Observable<Guitar[]>;
+  cardActionType = CardActionType;
   
   constructor(private homeService: HomeService) { }
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     MatIconModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
