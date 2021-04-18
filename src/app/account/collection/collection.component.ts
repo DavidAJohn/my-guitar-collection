12733f58747f6dd13c3f6166d500317d38a8dff0
David John <davidajohn@gmail.com>
2021-04-18 13:19:31 +0100

    Guitar collection page now shows user's collection

--- src/app/account/collection/collection.component.ts
+++ src/app/account/collection/collection.component.ts
@@ -1,9 +1,11 @@
+import { GuitarsService } from './../../shared/services/guitars.service';
 import { CardActionType } from './../../shared/components/guitar-card/guitar-card.component';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../account.service';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import { Guitar } from 'src/app/shared/models/guitar';
 
 @Component({
   selector: 'app-collection',
@@ -11,14 +13,29 @@ import 'firebase/auth';
   styleUrls: ['./collection.component.scss']
 })
 export class CollectionComponent implements OnInit {
-  loggedInUser$: Observable<firebase.User>;
+  currentFirebaseAuthPlayer$: Observable<firebase.User | null>;
+  ownerId: string = "";
+  guitars$: Observable<Guitar[] | null>;
   cardActionType = CardActionType;
 
-  constructor(private accountService: AccountService) {
-    this.loggedInUser$ = this.accountService.playerData$;
+  constructor(private accountService: AccountService, private guitarsService: GuitarsService) {
   }
 
   ngOnInit(): void {
+    this.getCurrentFirebaseAuthPlayer();
+  }
+
+  getCurrentFirebaseAuthPlayer() {
+    this.currentFirebaseAuthPlayer$ = this.accountService.getPlayer();
+
+    this.currentFirebaseAuthPlayer$
+      .subscribe((user: firebase.User) => {
+        this.ownerId = user.uid;
+
+        this.guitars$ = this.guitarsService.loadGuitarsForPlayerCollection(
+          10, "createdAt", "desc", this.ownerId
+        );
+      });
   }
 
 }
